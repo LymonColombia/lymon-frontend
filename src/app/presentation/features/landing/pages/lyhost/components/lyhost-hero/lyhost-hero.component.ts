7e1a905e07696c016bf95a611f599a657bbf60a7
T-cordoba <tommy.cormo@gmail.com>
2026-03-26 10:04:58 -0500

    LYMON-415-FE-Hacer-landing-page-de-LyHost
    replace SVG icons with ng-icons on hero component

--- src/app/presentation/features/landing/pages/lyhost/components/lyhost-hero/lyhost-hero.component.ts
+++ src/app/presentation/features/landing/pages/lyhost/components/lyhost-hero/lyhost-hero.component.ts
@@ -1,10 +1,13 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { bootstrapCalendar3, bootstrapGeoAlt, bootstrapPeople } from '@ng-icons/bootstrap-icons';
 import { ButtonComponent } from '@/presentation/shared/components/button/button.component';
 
 @Component({
   selector: 'app-lyhost-hero',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, NgIcon],
+  viewProviders: [provideIcons({ bootstrapGeoAlt, bootstrapCalendar3, bootstrapPeople })],
   templateUrl: './lyhost-hero.component.html',
   styleUrl: './lyhost-hero.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
