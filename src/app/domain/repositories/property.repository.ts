774cfc4220c0eb2a766d694d20c6200080355739
T-cordoba <tommy.cormo@gmail.com>
2026-05-13 17:20:51 -0500

    LYMON-857 feat(domain): add repository contracts and use cases for property CRUD

--- src/app/domain/repositories/property.repository.ts
+++ src/app/domain/repositories/property.repository.ts
@@ -1,7 +1,10 @@
 import { Observable } from 'rxjs';
-import { CreatePropertyDto, CreateUnitDto } from '@/domain/entities/property.model';
+import { CreatePropertyDto, CreateUnitDto, PropertyDetail, UpdatePropertyDto } from '@/domain/entities/property.model';
 
 export abstract class PropertyRepository {
   abstract createProperty(data: CreatePropertyDto): Observable<unknown>;
   abstract createUnit(data: CreateUnitDto): Observable<unknown>;
+  abstract getPropertyById(id: string): Observable<PropertyDetail>;
+  abstract updateProperty(id: string, data: UpdatePropertyDto): Observable<unknown>;
+  abstract deleteProperty(id: string): Observable<unknown>;
 }
