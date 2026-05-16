25ae0eb0d60c6bda5939b2a4e7cf6cd033fd4f51
T-cordoba <tommy.cormo@gmail.com>
2026-05-15 20:56:50 -0500

    LYMON-858 feat: add update and delete actions for property units with corresponding use cases

--- src/app/domain/repositories/property.repository.ts
+++ src/app/domain/repositories/property.repository.ts
@@ -1,5 +1,6 @@
 import { Observable } from 'rxjs';
-import { CreatePropertyDto, CreateUnitDto, PropertyDetail, UpdatePropertyDto, UpdateUnitMediaKeysDto } from '@/domain/entities/property.model';
+import { CreatePropertyDto, CreateUnitDto, PropertyDetail, UpdatePropertyDto, UpdateUnitDto, UpdateUnitMediaKeysDto } from '@/domain/entities/property.model';
+import { Unit } from '@/domain/entities/staff.model';
 
 export abstract class PropertyRepository {
   abstract createProperty(data: CreatePropertyDto): Observable<unknown>;
@@ -8,4 +9,7 @@ export abstract class PropertyRepository {
   abstract updateProperty(id: string, data: UpdatePropertyDto): Observable<unknown>;
   abstract deleteProperty(id: string): Observable<unknown>;
   abstract updateUnitMediaKeys(id: string, data: UpdateUnitMediaKeysDto): Observable<unknown>;
+  abstract updateUnit(id: string, data: UpdateUnitDto): Observable<unknown>;
+  abstract deleteUnit(id: string): Observable<unknown>;
+  abstract getUnitById(id: string): Observable<Unit>;
 }
